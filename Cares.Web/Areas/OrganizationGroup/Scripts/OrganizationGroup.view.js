f443e6cc68de8a2cdc3221dd412ea3109965abfd
Baqer Naqvi <link2naqvi@gmail.com>
2014-08-13 15:06:25 +0500

    fleetpool commit

---
+++ Cares.Web/Areas/OrganizationGroup/Scripts/OrganizationGroup.view.js
@@ -0,0 +1,40 @@
+﻿/*
+    View for the Product. Used to keep the viewmodel clear of UI related logic
+*/
+define("OrganizationGroup/OrganizationGroup.view",
+    ["jquery", "OrganizationGroup/OrganizationGroup.viewModel"], function ($, organizationGroupViewModel) {
+
+        var ist = window.ist || {};
+
+        // View 
+      
+        ist.OrganizationGroup.view = (function (specifiedViewModel) {
+            var
+                // View model 
+                viewModel = specifiedViewModel,
+                // Binding root used with knockout
+                bindingRoot = $("#OrgGroupRootBinding")[0],
+                // Initialize
+                initialize = function () {
+                    if (!bindingRoot) {
+                        return;
+                    }
+                    // Handle Sorting
+                  //  handleSorting("OrganizationGroupTable", viewModel.sortOn, viewModel.sortIsAsc, viewModel.getFleetPools);
+                };
+
+            initialize();
+
+
+            return {
+                bindingRoot: bindingRoot,
+                viewModel: viewModel
+            };
+        })(organizationGroupViewModel);
+
+        // Initialize the view model
+        if (ist.OrganizationGroup.view.bindingRoot) {
+            organizationGroupViewModel.initialize(ist.OrganizationGroup.view);
+        }
+        return ist.OrganizationGroup.view;
+    });
