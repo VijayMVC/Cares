5515ae99f17094231cd475e20b47c52619e33ef7
Baqer Naqvi <link2naqvi@gmail.com>
2014-08-27 11:42:12 +0500

    localization done

--- Cares.Web/Areas/Organization/Controllers/OrganizationController.cs
+++ Cares.Web/Areas/Organization/Controllers/OrganizationController.cs
@@ -2,23 +2,38 @@
 
 namespace Cares.Web.Areas.Organization.Controllers
 {
+    /// <summary>
+    /// Organization Controller
+    /// </summary>
     public class OrganizationController : Controller
     {
-
+        /// <summary>
+        /// Organization Group Area
+        /// </summary>
         public ActionResult OrganizationGroup()
         {
             return View();
         }
 
+        /// <summary>
+        /// Company Area
+        /// </summary>
         public ActionResult Company()
         {
             return View();
         }
 
+        /// <summary>
+        /// Department Area
+        /// </summary>
         public ActionResult Department()
         {
             return View();
         }
+
+        /// <summary>
+        /// Operation Area
+        /// </summary>
         public ActionResult Operation()
         {
             return View();
