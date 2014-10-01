d1a1f07688e0db6788607ad6480464eba3b65648
Baqer Naqvi <link2naqvi@gmail.com>
2014-10-01 17:21:00 +0500

    Business Legal Status all done

---
+++ Cares.Web/Areas/BusinessPartner/Models/BusinessLegalStatus.view.js
@@ -0,0 +1,33 @@
+﻿/*
+    View for the Business Legal Status Used to keep the viewmodel clear of UI related logic
+*/
+define("businessLegalStatus/businessLegalStatus.view",
+    ["jquery", "businessLegalStatus/businessLegalStatus.viewModel"], function ($, businessLegalStatusViewModel) {
+        var ist = window.ist || {};
+        // View 
+        ist.BusinessLegalStatus.view = (function (specifiedViewModel) {
+            var
+                // View model 
+                viewModel = specifiedViewModel,
+                // Binding root used with knockout
+                bindingRoot = $("#BusinessLegalStatusBinding")[0],
+                // Initialize
+                initialize = function () {
+                    if (!bindingRoot) {
+                        return;
+                    }
+                    // Handle Sorting
+                    handleSorting("OrgGroupTable", viewModel.sortOn, viewModel.sortIsAsc, viewModel.getbusinessLegalStatuses);
+                };
+            initialize();
+            return {
+                bindingRoot: bindingRoot,
+                viewModel: viewModel
+            };
+        })(businessLegalStatusViewModel);
+        // Initialize the view model
+        if (ist.BusinessLegalStatus.view.bindingRoot) {
+            businessLegalStatusViewModel.initialize(ist.BusinessLegalStatus.view);
+        }
+        return ist.BusinessLegalStatus.view;
+    });
