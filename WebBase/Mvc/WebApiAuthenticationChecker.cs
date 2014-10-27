3524fc5d7f7e055e09a8281b9b8e3499cacd0305
Mohammad Ali Raza <ali@innostark.com>
2014-10-27 20:00:47 +0500

    Web Api Project added

---
+++ WebBase/Mvc/WebApiAuthenticationChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cares.Interfaces.Security;
+
+namespace Cares.WebBase.Mvc
+{
+    public sealed class WebApiAuthenticationChecker : IWebApiAuthenticationChecker
+    {
+        public WebApiAuthenticationChecker()
+        {
+            
+        }
+        public bool Check(string userName, string password)
+        {
+            return true;
+        }
+    }
+}
