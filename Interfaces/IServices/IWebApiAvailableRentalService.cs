93fb3892dbcbddaedb7f39b8deea03a1d561ac81
Mohammad Ali Raza <ali@innostark.com>
2014-10-30 16:19:57 +0500

    Web Api Authentication updated
    Web Api for Available HireGroup included

---
+++ Interfaces/IServices/IWebApiAvailableRentalService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Cares.Models.ResponseModels;
+
+namespace Cares.Interfaces.IServices
+{
+    /// <summary>
+    /// Web Api Service for getting available items for Rental
+    /// </summary>
+    public interface IWebApiAvailableRentalService
+    {
+        /// <summary>
+        /// Get available Hire groups with rates for given location and duration
+        /// </summary>
+        IEnumerable<WebApiAvailaleHireGroup> GetAvailableHireGroupsWithRates(long operationWorkplaceId, DateTime startDateTime,
+            DateTime endDateTime, long domainKey);
+    }
+}
