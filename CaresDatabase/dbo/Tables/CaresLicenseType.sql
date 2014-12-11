e8511cd122a81bb4b1a2ec16d18142dc2e299911
Mohammad Ali Raza <ali@innostark.com>
2014-12-11 15:40:17 +0500

    database project updated for newly added tables

---
+++ CaresDatabase/dbo/Tables/CaresLicenseType.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[CaresLicenseType] (
+    [LicenseTypeId]    INT            NOT NULL,
+    [Name]             NVARCHAR (255) NULL,
+    [Price]            FLOAT (53)     NOT NULL,
+    [RowVersion]       BIGINT         CONSTRAINT [DF_CaresLicenseType_RowVersion] DEFAULT ((0)) NOT NULL,
+    [IsActive]         BIT            CONSTRAINT [DF_CaresLicenseType_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsPrivate]        BIT            CONSTRAINT [DF_CaresLicenseType_IsPrivate] DEFAULT ((0)) NOT NULL,
+    [IsReadOnly]       BIT            CONSTRAINT [DF_CaresLicenseType_IsReadOnly] DEFAULT ((0)) NOT NULL,
+    [IsDeleted]        BIT            CONSTRAINT [DF_CaresLicenseType_IsDeleted] DEFAULT ((0)) NOT NULL,
+    [RecCreatedDt]     DATETIME       NOT NULL,
+    [RecCreatedBy]     NVARCHAR (100) NOT NULL,
+    [RecLastUpdatedDt] DATETIME       NOT NULL,
+    [RecLastUpdatedBy] NVARCHAR (100) NOT NULL,
+    [UserDomainKey]    BIGINT         NOT NULL,
+    CONSTRAINT [PK_CaresLicenseType] PRIMARY KEY CLUSTERED ([LicenseTypeId] ASC)
+);
+
---
+++ CaresDatabase/dbo/Tables/LicenseDetailsDefault.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[LicenseDetailsDefault] (
+    [LicenseDetailsId] INT            NOT NULL,
+    [LicenseTypeId]    INT            NOT NULL,
+    [RAPerMonth]       INT            NOT NULL,
+    [Employee]         INT            NOT NULL,
+    [Branches]         INT            NOT NULL,
+    [FleetPools]       INT            NOT NULL,
+    [Vehicles]         INT            NOT NULL,
+    [RowVersion]       BIGINT         CONSTRAINT [DF_LicenseDetailsDefault_RowVersion] DEFAULT ((0)) NOT NULL,
+    [IsActive]         BIT            CONSTRAINT [DF_LicenseDetailsDefault_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsPrivate]        BIT            CONSTRAINT [DF_LicenseDetailsDefault_IsPrivate] DEFAULT ((0)) NOT NULL,
+    [IsReadOnly]       BIT            CONSTRAINT [DF_LicenseDetailsDefault_IsReadOnly] DEFAULT ((0)) NOT NULL,
+    [IsDeleted]        BIT            CONSTRAINT [DF_LicenseDetailsDefault_IsDeleted] DEFAULT ((0)) NOT NULL,
+    [RecCreatedDt]     DATETIME       NOT NULL,
+    [RecCreatedBy]     NVARCHAR (100) NOT NULL,
+    [RecLastUpdatedDt] DATETIME       NOT NULL,
+    [RecLastUpdatedBy] NVARCHAR (100) NOT NULL,
+    [UserDomainKey]    BIGINT         NOT NULL,
+    CONSTRAINT [LicenseDetailsDefault_pk] PRIMARY KEY CLUSTERED ([LicenseDetailsId] ASC),
+    CONSTRAINT [LicenseDetailsDefault_fk] FOREIGN KEY ([LicenseTypeId]) REFERENCES [dbo].[CaresLicenseType] ([LicenseTypeId]) ON DELETE CASCADE
+);
+
