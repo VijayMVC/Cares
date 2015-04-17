24024dad4e90ee445dfdb91f019e17a400291fa8
Mohammad Ali Raza <ali@innostark.com>
2015-04-17 20:23:42 +0500

    RaQueue searching fixed

--- Cares.Web/Resources/RentalAgreement/RentalAgreement.Designer.cs
+++ Cares.Web/Resources/RentalAgreement/RentalAgreement.Designer.cs
@@ -457,7 +457,7 @@ namespace Cares.Web.Resources.RentalAgreement {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to End Date.
+        ///   Looks up a localized string similar to &lt;End Date&gt;.
         /// </summary>
         public static string EndDate {
             get {
